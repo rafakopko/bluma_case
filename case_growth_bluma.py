d65a28d553186bba840826d85a5f04fc1fd14ead
Rafael <rafael.kopko@gmail.com>
2025-07-25 11:18:02 -0300

    feat: Initialize Streamlit app configuration and add required dependencies

--- case_growth_bluma.py
+++ case_growth_bluma.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import numpy as np
 
 # Configuração da página
